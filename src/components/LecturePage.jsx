8f6a4d0aec1d368b1e905595c24c9fde8859746c
oravid <oravid85@gmail.com>
2025-09-26 11:49:51 +0300

    feat: add artist details and update LecturePage layout for improved presentation

--- src/components/LecturePage.jsx
+++ src/components/LecturePage.jsx
@@ -6,25 +6,19 @@ export function LecturePage(props) {
   return (
     <div>
       <h1>{lectureData.header}</h1>
-
-      <h2 className="text-center">על ההרצאה</h2>
-      <p className="text-justify">
-        אריס סאן, הוא אחד מאבות המוזיקה והתרבות הישראלית וחלוץ המוזיקה
-        הים-תיכונית בארץ.
-      </p>
-      <p className="text-justify">
-        מי שהגיע ליפו כנער יווני בן17 והתחיל לנגן גיטרה במועדונים, והפך לאחת
-        מאושיות התרבות הגדולות ביותר בישראל בשנות ה-60 וה-70, 'הוליד' דורות של
-        מוזיקאים חדשים שהביאו את הקולות והניחוחות של הים התיכון והמזרח לקדמת
-        הבמה בישראל.
-      </p>
-
-      <p className="text-justify">
-        שעה של סיפורים, סרטונים, שירים, מוזיקה חיה ותמונות שבה נכיר את אריס סאן,
-        דמותו, הסיפור שלו, מאיפה בא, ולאן הלך? וננסה להבין איך הצליח ב-13 שנים
-        להשאיר חותם על ישראל, ולהפוך לפרק בסיפור שלה, שמלווה אותה עד היום ונוכח
-        כמעט בכל פינה.
-      </p>
+      <br />
+      <br />
+      <img src={lectureData.picture} />
+      <br />
+      <br />
+      <h2 className="text-center">על האומן</h2>
+      <br />
+      <p className="text-justify">{lectureData.onartist}</p>
+      <br />
+      <br />
+      <h2>על ההרצאה</h2>
+      <br />
+      <p className="text-justify">{lectureData.onlecture}</p>
     </div>
   );
 }
