acaf65d8e41910e5ce52bbe69c29276f6d3369e0
oravid <oravid85@gmail.com>
2025-10-23 12:11:15 +0300

    feat: enhance RecomCar component with mainview support and improved slide navigation

--- src/components/RecomCar.jsx
+++ src/components/RecomCar.jsx
@@ -1,66 +1,71 @@
 import { useState } from "react";
 
-export default function RecomCar({ items }) {
+export default function RecomCar({
+  items,
+  mainview,
+  lectureData = {},
+  aboutMe = {},
+}) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
-    setCurrentIndex((prev) => (prev === items.length - 1 ? 0 : prev + 1));
+    setCurrentIndex((prev) => (prev + 1) % items.length);
   };
 
   const prevSlide = () => {
     setCurrentIndex((prev) => (prev === 0 ? items.length - 1 : prev - 1));
   };
-  const maxDots = 3;
-  const visibleDotIndex =
-    currentIndex - Math.floor(currentIndex / maxDots) * maxDots;
 
+  const bgcolor =
+    mainview === "Greeting"
+      ? "bg-white"
+      : mainview === "AboutMe"
+      ? aboutMe.bgColor
+      : lectureData.bgColor;
+
+  console.log("bgcolor", bgcolor, lectureData, aboutMe);
   return (
-    <div className="relative w-full max-w-xl mx-4 overflow-hidden rounded-2xl shadow-lg mb-2 bg-amber-100">
-      <div
-        className="flex transition-transform duration-500 ease-in-out"
-        style={{ transform: `translateX(+${currentIndex * 100}%)` }}
-      >
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="w-full flex-shrink-0 flex flex-col gap-4 items-center justify-center bg-amber-100 h-64"
-          >
-            <p className="text-2xl md:text-xl text-center font-medium text-amber-800 px-4">
-              {item.text}
-            </p>
-            {item.author && (
-              <p className="text-xl md:text-base flex justify-end mt-2 text-gray-600">
-                {item.author}
+    <div className={`${bgcolor} w-full flex justify-center mb-4 md:mb-0`}>
+      <div className="flex flex-col relative h-[360px] w-fit max-w-xl mx-4 overflow-hidden rounded-2xl shadow-lg bg-amber-100">
+        <div
+          className="h-full flex transition-transform duration-500 ease-in-out shadow-lg"
+          style={{ transform: `translateX(+${currentIndex * 100}%)` }}
+        >
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="w-full flex-shrink-0 flex flex-col gap-4 items-center justify-center bg-amber-100 h-64"
+            >
+              <p className="text-2xl md:text-xl text-center font-medium text-amber-800 px-4">
+                {item.text}
               </p>
-            )}
-          </div>
-        ))}
-      </div>
-
-      <button
-        onClick={prevSlide}
-        className="absolute bottom-4 right-4 border-2 border-amber-700 bg-amber-600 rounded-2xl shadow  p-2  font-heebo text-base text-white  hover:bg-amber-700 hover:font-bold transition-all duration-200"
-      >
-        {"קודם"}
-      </button>
-
-      <button
-        onClick={nextSlide}
-        className="absolute bottom-4 left-4  border-2 border-amber-700 bg-amber-600 rounded-2xl shadow  p-2  font-heebo text-base text-white hover:bg-amber-700 hover:font-bold transition-all duration-200"
-      >
-        {"הבא"}
-      </button>
-
-      <div className="  bg-amber-100 absolute bottom-1 w-full flex justify-center gap-x-2">
-        {Array.from({ length: maxDots }).map((_, index) => (
+              {item.author && (
+                <p className="text-xl md:text-base flex justify-end mt-2 text-gray-600">
+                  {item.author}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="px-4 py-2 absolute bottom-0 w-full flex flex-row items-center justify-between">
           <button
-            key={index}
-            onClick={() => setCurrentIndex(index)}
-            className={`w-3 h-3 rounded-full ${
-              index === visibleDotIndex ? "bg-amber-700" : "bg-amber-400"
-            }`}
-          />
-        ))}
+            onClick={prevSlide}
+            className=" border-2 border-amber-700 bg-amber-600 rounded-2xl shadow  p-2  font-heebo text-base text-white  hover:bg-amber-700 hover:font-bold transition-all duration-200"
+          >
+            {"קודם"}
+          </button>
+          <div>
+            <span className="mx-1">{items.length}</span>
+            <span className="mx-1">/</span>
+            <span className="mx-1">{currentIndex + 1}</span>
+          </div>
+          <button
+            onClick={nextSlide}
+            className="  border-2 border-amber-700 bg-amber-600 rounded-2xl shadow  p-2  font-heebo text-base text-white hover:bg-amber-700 hover:font-bold transition-all duration-200"
+          >
+            {"הבא"}
+          </button>
+        </div>
       </div>
     </div>
   );
