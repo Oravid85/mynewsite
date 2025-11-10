ca3ca2b0cf6c4b627dd098a9a191adfc9d49c0f8
oravid <oravid85@gmail.com>
2025-11-10 21:11:50 +0200

    fixed?

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import NotFoundPage from "./pages/NotFoundPage";
 import { TestPage } from "./pages/TestPage";
 
 import { LecturePage } from "./components/LecturePage";
-import { AboutMe } from "./components/AboutMe.jsxboutMe";
+import { AboutMe } from "./components/AboutMe.jsx";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import ContactForm from "./components/form.jsx";
